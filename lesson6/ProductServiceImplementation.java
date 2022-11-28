0979c3bb24b12b5e358bc7baef9a879acdd95f70
Pandatokyo <99756016+Pandatokyo@users.noreply.github.com>
2022-11-28 22:15:07 +0300

    homework6

---
+++ lesson6/ProductServiceImplementation.java
@@ -0,0 +1,39 @@
+package lesson6;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class ProductServiceImplementation implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getProductList() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product searchById(Long id) {
+        return productRepository.searchById(id);
+    }
+
+    @Override
+    public void Update(Product product) {
+        productRepository.Update(product);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+}
